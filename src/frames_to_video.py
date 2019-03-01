9d2e659b405d61edbcf0f040d696221108006cd5
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2019-03-01 20:00:27 +0530

    validating the issue when non existing the required output location

--- src/frames_to_video.py
+++ src/frames_to_video.py
@@ -16,7 +16,7 @@ import cv2
 import os
 from os.path import isfile, join
 
-def convert_frames_to_video(input_frames_path, output_video_path, fps):
+def convert_frames_to_video(input_frames_path, output_video_path, output_video_name, fps):
 
     if (os.path.isdir(input_frames_path)):
 
@@ -28,6 +28,14 @@ def convert_frames_to_video(input_frames_path, output_video_path, fps):
         files.sort(key=lambda x: int(x[5:-4]))
 
         if(len(files)!=0):
+
+            try:
+                # validate the exitence of the output location to save the output video
+                if not os.path.exists(output_video_path):
+                    os.makedirs(output_video_path)
+            except OSError:
+                print('Error: Creating directory of data')
+
             for i in range(len(files)):
                 # reading image files
                 filename = input_frames_path + files[i]
@@ -40,7 +48,7 @@ def convert_frames_to_video(input_frames_path, output_video_path, fps):
                 frame_array.append(img)
 
             # Initiate the output video file
-            out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+            out = cv2.VideoWriter(output_video_path+output_video_name, cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
 
             for i in range(len(frame_array)):
                 # writing to a image array
@@ -52,10 +60,11 @@ def convert_frames_to_video(input_frames_path, output_video_path, fps):
         print("Given path of the frames not exists.")
 
 def run():
-    input_frames_path = "./data/generated_frame/"
-    output_video_path = "./data/output_video/output_video.avi"
+    input_frames_path = "./data/generated_frames/"
+    output_video_path = "./data/output_video/"
+    output_video_name = "output_video.avi"
     fps = 25.0
-    convert_frames_to_video(input_frames_path, output_video_path, fps)
+    convert_frames_to_video(input_frames_path, output_video_path, output_video_name, fps)
 
 if __name__ == "__main__":
     run()
