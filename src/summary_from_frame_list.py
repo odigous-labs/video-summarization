acc8f0b785e18f0c017474370af2fa7c68c02516
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2019-03-03 00:49:25 +0530

    changing the variables of function summary_from_frame_list relevant to the change of input dictionary

--- src/summary_from_frame_list.py
+++ src/summary_from_frame_list.py
@@ -6,9 +6,11 @@ def summary_from_frame_list(frame_list_with_objects, threshold_of_objects_in_fra
         obj_list = frame_list_with_objects[list(frame_list_with_objects.keys())[i]]
 
         for j in range(threshold_of_objects_in_frame):
-            if (obj_list[j][0] not in object_array):
-                object_array.append(obj_list[j][0])
-                if (i not in summary_frames):
-                    summary_frames.append(i)
+            if (obj_list[j] not in object_array):
+                object_array.append(obj_list[j])
+
+                frame_name = list(frame_list_with_objects.keys())[i]
+                if (frame_name not in summary_frames):
+                    summary_frames.append(frame_name)
 
     return summary_frames
